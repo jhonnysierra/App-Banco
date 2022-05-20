a8057abd1a50100e02c341353eb4e9860abf7adf
jhonnysierrap <jhonnysierrap@gmail.com>
2022-05-20 13:49:35 -0500

    Update inversion repository
    Se crea metodo para obtener el saldo incial de la inversion

--- cliente/src/main/java/com/microservicio/cliente/repository/InversionRepository.java
+++ cliente/src/main/java/com/microservicio/cliente/repository/InversionRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -18,6 +19,7 @@ public interface InversionRepository extends JpaRepository<Inversion, Long> {
 
     /**
      * Consulta que trae las inversiones activas del cliente
+     *
      * @param idCliente Identificador del cliente
      * @return Lista con las inversiones del cliente
      */
@@ -25,11 +27,21 @@ public interface InversionRepository extends JpaRepository<Inversion, Long> {
             "where i.cliente.id=:idCliente and i.fechaFinPortafolio=null")
     public List<Inversion> consultarInversionesCliente(@Param("idCliente") Long idCliente);
 
+    /**
+     * Consulta que permite saber el saldo inicial de una inversion
+     *
+     * @param idInversion Identificador de la inversion
+     * @param fechaActivacion Fecha de activacion de la inversion
+     * @return Valor inicial invertido
+     */
+    @Query(value = "SELECT CONSULTAR_SALDO_INICIAL(:idInversion,:fechaActivacion) FROM DUAL", nativeQuery = true)
+    public BigDecimal obtenerSaldoInicial(@Param("idInversion") Long idInversion, @Param("fechaActivacion") LocalDate fechaActivacion);
 
     //HACER ESTA GRAN PERRA CON NATIVE QUERY
     /*
     @Query(value = "SELECT SUM(i.saldo) FROM Inversion i " +
             "WHERE i.cliente.id=:idCliente AND i.fechaFinPortafolio=null")
+            //SELECT i.saldo FROM inversiones i WHERE i.cliente_id=1 AND i.fecha_fin_portafolio IS NULL
     public BigDecimal consultarSaldo(@Param("idCliente") Long idCliente);
     */
 
